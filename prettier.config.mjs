046017b6cbaf0e5d96147270f6e518351424f0fb
陆子虚 <lurongv@qq.com>
2025-12-16 16:11:54 +0800

    🐞 fix: 修复语法错误和导入顺序

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -9,14 +9,6 @@ const config = {
     arrowParens: "avoid",
     endOfLine: "lf",
     printWidth: 160,
-    overrides: [
-        {
-            files: "*.mdc",
-            options: {
-                parser: "markdown",
-            },
-        },
-    ],
     plugins: ["@1adybug/prettier"],
     controlStatementBraces: "add",
     multipleLineBraces: "add",
